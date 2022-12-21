49493db6c48ecb5606a28535eca841a16f7024a3
mikalikahi <sweeney.phil@gmail.com>
2022-12-21 15:38:02 +0800

    Modified appearance of template in thymeleaf with css and js example

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -2,3 +2,17 @@ function greeting() {
     alert("Hello Spring Developer!!");
 }
 
+function changeHeadingColor() {
+    let allHeadings =document.getElementsByName("heading");
+    console.log(allHeadings);
+    for (let i = 0; i < allHeadings.length; i++) {
+        console.log(allHeadings[i].style.color === "rgb(255, 0, 0)");
+        if(allHeadings[i].style.color === "rgb(255, 0, 0)"){
+            allHeadings[i].style.color = "black";
+        } else {
+            allHeadings[i].style.color = "#FF0000";
+        }
+
+    }
+}
+
